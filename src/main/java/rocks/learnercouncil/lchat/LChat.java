78d71bce086e96771082859e2543110780d2e96d
H2ofiremaster <h2ofiremaster@gmail.com>
2023-03-20 11:08:39 -0700

    Initial commit

---
+++ src/main/java/rocks/learnercouncil/lchat/LChat.java
@@ -0,0 +1,17 @@
+package rocks.learnercouncil.lchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
